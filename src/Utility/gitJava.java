18cf2fde265a94365f3a3479c913f80cb1dc79e9
Muhammed Cakmak <65066409+muhacakmak@users.noreply.github.com>
2020-08-05 19:38:10 -0600

    Update gitJava.java

--- src/Utility/gitJava.java
+++ src/Utility/gitJava.java
@@ -3,6 +3,8 @@ package Utility;
 public class gitJava {
     public static void main(String[] args) {
 
-        System.out.println("Loving learning java, I think java is number one");
+        System.out.println("Love learning java, I think java is number one");
+        
+        System.out.println("I love coding")
     }
 }
