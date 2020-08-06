79f6cea7cd1bb0e7510f80c4c4a8bba4ed0189d8
muhacakmak <muhammeddcakmak@outlook.com>
2020-08-05 18:02:01 -0600

    Hey git

---
+++ src/Utility/ShoppingList.java
@@ -0,0 +1,35 @@
+package Utility;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ShoppingList {
+
+    // write your code here
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String shoppingListReport = "";
+        String item = "";
+        String countinue = "";
+        double price = 0;
+        int count = 1;
+        double totalPrice = 0;
+
+        do{
+            System.out.println("Enter Item"+count+" and its price:");
+            item = scan.next();
+            price = scan.nextDouble();
+            System.out.println("Add one more item?");
+            countinue = scan.next();
+            shoppingListReport += "Item"+count+": "+item+" Price: "+price+", ";
+            totalPrice += price;
+            if(count>10){
+                break;
+            }
+            count++;
+        }while(countinue.equalsIgnoreCase("yes"));
+
+        System.out.println(shoppingListReport.substring(0,shoppingListReport.length()-2));
+        System.out.println("Total price: "+totalPrice);
+    }
+}
